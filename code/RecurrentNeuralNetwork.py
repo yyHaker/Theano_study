d30f5a13a94d02b571e091fd0e41fbe0f25c1bec
yyhaker <572176750@qq.com>
2017-11-07 16:32:15 +0800

    implement the rnn sgd

--- code/RecurrentNeuralNetwork.py
+++ code/RecurrentNeuralNetwork.py
@@ -58,7 +58,7 @@ class RNN(object):
             # Note that we are indexing U by x_t. This is the same as multiplying U with a one-hot vector.
             s_t = T.tanh(U[:, x_t] + T.dot(W, s_t_pre))
             o_t = T.nnet.softmax(T.dot(V, s_t))
-            return [o_t[0], s_t]
+            return [o_t[0], s_t]               # why 0_t[0] ?
         [o, s], updates = theano.scan(fn=forward_prop_step, sequences=x,
                                       outputs_info=[None, dict(initial=T.zeros(self.hidden_dim))],
                                       non_sequences=[U, V, W], truncate_gradient=self.bptt_truncate,
@@ -66,7 +66,7 @@ class RNN(object):
         # get the index of highest score
         prediction = T.argmax(o, axis=1)
         # cross-entropy
-        o_error = T.nnet.categorical_crossentropy(o, y)
+        o_error = T.sum(T.nnet.categorical_crossentropy(o, y))
 
         # gradients
         dU = T.grad(o_error, U)
@@ -150,7 +150,63 @@ def gradient_check_theano(model, x, y, h=0.001, error_thershold=0.01):
         print "Gradient check for parameters %s passed." % pname
 
 
+def save_model_parameters_theano(outfile, model):
+    U, V, W = model.U.get_value(), model.V.get_value(), model.W.get_value()
+    np.savez(outfile, U=U, V=V, W=W)
+    print "Saved model parameters to %s." % outfile
+
+
+def load_model_parameters_theano(path, model):
+    npzfile = np.load(path)
+    U, V, W = npzfile["U"], npzfile["V"], npzfile["W"]
+    model.hidden_dim = U.shape[0]
+    model.word_dim = U.shape[1]
+    model.U.set_value(U)
+    model.V.set_value(V)
+    model.W.set_value(W)
+    print "Loaded model parameters from %s. hidden_dim=%d word_dim=%d" % (path, U.shape[0], U.shape[1])
+
+
+def train_with_sgd(model, X_train, Y_train, learning_rate=0.005, nepoch=1, evaluate_loss_after=5):
+    """
+
+    :param model: an RNN object
+    :param X_train: a set of sentences and every sentence is tokenized, i.e. [[0, 179, 341, 416]]
+    :param Y_train: same as X_train but shift by one position, i.e. [[179, 341, 416, 1]]
+    :param learning_rate: float , learning_rate
+    :param nepoch:
+    :param evaluate_loss_after:
+    :return:
+    """
+    losses = []
+    num_examples_seen = 0
+    for epoch in range(nepoch):
+        # optionally evaluate the loss
+        if epoch % evaluate_loss_after == 0:
+            loss = model.calculate_loss(X_train, Y_train)
+            losses.append((num_examples_seen, loss))
+
+            time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+            print "%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss)
+
+            # Adjust the learning rate if loss increases
+            if len(losses) > 1 and losses[-1][1] > losses[-2][1]:
+                learning_rate = learning_rate * 0.5
+                print "setting learning rate to %f " % learning_rate
+            sys.stdout.flush()
+
+            # saving model parameters
+            save_model_parameters_theano("./data/rnn-theano-%d-%d-%s.npz" % (model.hidden_dim, model.word_dim, time), model)
+        # for each training example
+        for i in range(len(Y_train)):
+            # One SGD step
+            model.sgd_step(X_train[i], Y_train[i], learning_rate)
+            num_examples_seen += 1
+
+
 def test_rnn():
+
+    nltk.download("book")
 
     _VOCABULARY_SIZE = int(os.environ.get('VOCABULARY_SIZE', '8000'))
     _HIDDEN_DIM = int(os.environ.get('HIDDEN_DIM', '80'))
@@ -165,10 +221,73 @@ def test_rnn():
 
     # read data and append SENTENCE_START and SENTENCE_END tokens
     print "Reading CSV file"
-    with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+    with open('../data/reddit-comments-2015-08.csv', 'rb') as f:
         reader = csv.reader(f, skipinitialspace=True)
         reader.next()
         # split full comments into sentences
+        sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+        # append SENTENCE_START, SENTENCE_END tokens
+        sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+    print "Parsed %d sentences ." % (len(sentences))
+    print ".......sample sentences"
+    print sentences[0]
+    print sentences[1]
+    print sentences[2]
+    print sentences[3]
+
+    # Tokenize the sentences into words
+    tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+    print ".......tokenized_sentences"
+    print tokenized_sentences[0]
+    print tokenized_sentences[1]
+    print tokenized_sentences[2]
+    print tokenized_sentences[3]
+
+    # count the word frequencies
+    word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+    print "Found %d unique words tokens. " % len(word_freq.items())
+    # print word_freq
+    # print word_freq.items()
+
+    # get the most common words and build index_to_word and word_to_index vectors
+    vocab = word_freq.most_common(vocabulary_size - 1)
+    print "The top ten vocab: ", vocab[: 10]
+    index_to_word = [x[0] for x in vocab]
+    index_to_word.append(unknown_token)
+    word_to_index = dict([(w, i)for i, w in enumerate(index_to_word)])
+
+    print "Using vocabulary_size %d. " % vocabulary_size
+    print "The least frequent word in our vocabulary is '%s' and appeared '%d' times." % (vocab[-1][0], vocab[-1][1])
+
+    # replace all words not in our vocabulary with the unknown token
+    for i, sent in enumerate(tokenized_sentences):
+        tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+
+    # create the training data
+    X_train = np.asarray([[word_to_index[w] for w in sent[: -1]] for sent in tokenized_sentences])
+    Y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+    print "....sampling two train data:"
+    print "X_train[0]: ", X_train[0]
+    print "Y_train[0]: ", Y_train[0]
+
+    model = RNN(word_dim=vocabulary_size, hidden_dim=_HIDDEN_DIM)
+    t1 = time.time()
+    model.sgd_step(X_train[10], Y_train[10], _LEARNING_RATE)
+    t2 = time.time()
+    print "SGD Step time: %f milliseconds " % (t2 - t1) * 1000.
+
+    if _MODEL_FILE !=None:
+        load_model_parameters_theano(_MODEL_FILE, model)
+
+    train_with_sgd(model, X_train, Y_train, nepoch=_NEPOCH, learning_rate=_LEARNING_RATE)
+
+
+if __name__ == '__main__':
+    test_rnn()
+
+
+
 
 
 
